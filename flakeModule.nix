2f7db73c78778f892f14531f7a77c5d5b929a5a0
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-01-03 23:44:38 +0100

    test

--- flakeModule.nix
+++ flakeModule.nix
@@ -2,16 +2,12 @@
   config,
   lib,
   inputs,
-  flake-parts-lib,
   ...
 }:
 let
   inherit (lib)
     mkOption
     types
-    ;
-  inherit (flake-parts-lib)
-    mkSubmoduleOptions
     ;
 in
 {
